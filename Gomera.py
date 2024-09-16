b2fb137d8bf8ff9bf0959ea2a2cc10c8a56ea41d
Angel_Gomera0917 <antoniogomera12@gmail.com>
2024-09-16 00:32:14 -0400

    Nuevo Cambio 2
    Algunas cosas

--- Gomera.py
+++ Gomera.py
@@ -1,2 +1,2 @@
 print("Darli es la mejor maestra de English.")
-print("Angel adora pasar todo el tiempo de su vida con esta adorable tan mujer")
+print("Angel adora pasar todo el tiempo de su vida con esta adorable tan mujer bella")
