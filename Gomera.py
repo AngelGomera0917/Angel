41e2d1505b7fd07ba2c9a0958a464e12b5c4c6b0
Angel_Gomera0917 <antoniogomera12@gmail.com>
2024-10-03 16:38:38 -0400

    agregue lo del planeta
    nuevo cambio

--- Gomera.py
+++ Gomera.py
@@ -1,2 +1,2 @@
-print("Darli es la mejor maestra de English.")
+print("Darli es la mejor maestra de English en el planeta.")
 print("Angel adora pasar todo el tiempo de su vida con esta adorable tan mujer bella")
